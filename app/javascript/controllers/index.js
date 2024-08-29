a121ac09e57054b6a5af39dea7b46caf94d272f1
frank_johnette <frank@syncromsp.com>
2024-08-29 17:14:44 -0500

    Add button to copy invite link

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,11 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import Clipboard from '@stimulus-components/clipboard'
+
+// Load all the controllers within this directory and all subdirectories. 
+// Controller files must be named *_controller.js.
 eagerLoadControllersFrom("controllers", application)
+
+// Register the Clipboard controller
+application.register('clipboard', Clipboard)
