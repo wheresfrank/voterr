ae0f8203683e94dcd1f930d6ba6c500b9b3ae5fa
frank_johnette <frank@syncromsp.com>
2024-10-30 14:06:37 -0500

    update sidekiq client config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,7 @@ require 'sidekiq'
 Sidekiq.configure_server do |config|
     config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
+
+Sidekiq.configure_client do |config|
+    config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
