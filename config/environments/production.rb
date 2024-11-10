ce5e8aa2bcf6c432f0ae98eb7f76c04bfb837d34
frank_johnette <frank@syncromsp.com>
2024-11-09 22:04:27 -0500

    update production redis config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,21 +48,6 @@ Rails.application.configure do
     /https:\/\/voterr.*/
   ]
 
-  # Add this configuration for Action Cable Redis adapter
-  config.action_cable.backend_redis_config = {
-    url: ENV.fetch("REDIS_URL"),
-    ssl_params: { 
-      verify_mode: OpenSSL::SSL::VERIFY_NONE
-    }
-  }
-
-  # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
-  # config.assume_ssl = true
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
-
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
