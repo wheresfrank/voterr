533a88c6206212773abc9f6eefa24b0a8e26b499
frank_johnette <frank@syncromsp.com>
2024-11-09 22:02:03 -0500

    More redis changes for heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 require 'redis'
 
 if Rails.env.production?
-  Redis.current = Redis.new(
+  $redis = Redis.new(
     url: ENV.fetch("REDIS_URL"),
     ssl_params: { 
       verify_mode: OpenSSL::SSL::VERIFY_NONE
