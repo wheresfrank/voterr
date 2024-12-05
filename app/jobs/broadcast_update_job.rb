d03010ce6a60c10dd458b2e056c15b446167a5a4
frank_johnette <frank@syncromsp.com>
2024-12-04 20:34:45 -0500

    fix vote count broadcast

--- app/jobs/broadcast_update_job.rb
+++ app/jobs/broadcast_update_job.rb
@@ -3,11 +3,21 @@ class BroadcastUpdateJob < ApplicationJob
 
   def perform(session_id)
     session = Session.find(session_id)
+    
+    # Broadcast voters panel update
     Turbo::StreamsChannel.broadcast_update_to(
       session,
       target: "voters-session-#{session.id}",
       partial: "sessions/voters",
       locals: { session: session }
+    )
+
+    # Broadcast session panel update
+    Turbo::StreamsChannel.broadcast_update_to(
+      session,
+      target: "session_panel_#{session.id}",
+      partial: "sessions/voting_stats",
+      locals: { session: session, user: session.user }
     )
   end
 end
