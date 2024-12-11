4fdecdba0a0df5198424e5ec3d1c92b39df64460
frank_johnette <frank@syncromsp.com>
2024-12-11 00:30:07 -0500

    solid queue in puma

--- config/puma.rb
+++ config/puma.rb
@@ -34,7 +34,7 @@ port ENV.fetch("PORT", 3000)
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma
-# plugin :solid_queue
+plugin :solid_queue
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
