7d027201a98a93ce1ac710264e25a53573053d59
frank_johnette <frank@syncromsp.com>
2024-12-11 02:03:33 -0500

    add url in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_controller.default_url_options = { host: "voterr.tv", protocol: 'https' }
+  config.action_mailer.default_url_options = { host: "voterr.tv", protocol: 'https' }
 end
